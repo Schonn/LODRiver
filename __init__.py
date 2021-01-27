cbf131259438d67eb2c788d73942d7134d0a7a9a
Pierre <hirs0054@gmx.com>
2021-01-27 18:54:31 +1030

    added enable/disable effect for active collection
    assistance with instancing objects - it seems very difficult to instance something that is not displayed

--- __init__.py
+++ __init__.py
@@ -39,8 +39,11 @@ class LODRIVER_PT_LodPanel(bpy.types.Panel):
     bpy.types.Scene.LODRIVERSpacing = bpy.props.FloatProperty(name="LOD Object Spacing",description="The amount of distance between a change in level of detail",default=10,min=0,max=90000)
 
     def draw(self, context):
+        self.layout.operator('lodriver.parenttoempties', text ='Parent Selected Objects to Empties')
         self.layout.operator('lodriver.createdrivers', text ='Create Visibility Drivers for Selected')
         self.layout.prop(context.scene,"LODRIVERSpacing",slider=False)
+        self.layout.operator('lodriver.visibleoverrideon', text ='Visible Override on Active Collection')
+        self.layout.operator('lodriver.visibleoverrideoff', text ='No Visible Override on Active Collection')
 
 #function to add properties and drivers to selected objects to make them hide and show in renders and viewport based on distance to empty
 class LODRIVER_OT_CreateDrivers(bpy.types.Operator):
@@ -49,8 +52,6 @@ class LODRIVER_OT_CreateDrivers(bpy.types.Operator):
     bl_description = "Add a driver to the selected object that enables and disables visibility at certain distances from a reference object"
     
     def execute(self, context):
-        print("create drivers")
-        
         #store selected objects
         selectedObjects = bpy.context.selected_objects
         #create distance reference empty if it does not exist
@@ -67,6 +68,7 @@ class LODRIVER_OT_CreateDrivers(bpy.types.Operator):
             lodObject = selectedObjects[lodObjectNumber]
             lodObject["LODRIVER_MINDISTANCE"] = lodObjectNumber * lodSpacingDistance
             lodObject["LODRIVER_MAXDISTANCE"] = (lodObjectNumber * lodSpacingDistance) + lodSpacingDistance
+            lodObject["LODRIVER_SWITCHOFF"] = 0
             viewportDriver = lodObject.driver_add("hide_viewport").driver
             viewportDriverMaxVar = viewportDriver.variables.new()
             viewportDriverMaxVar.name = "LODRIVERMAX"
@@ -76,26 +78,79 @@ class LODRIVER_OT_CreateDrivers(bpy.types.Operator):
             viewportDriverMinVar.name = "LODRIVERMIN"
             viewportDriverMinVar.targets[0].id = lodObject
             viewportDriverMinVar.targets[0].data_path = '["LODRIVER_MINDISTANCE"]'
+            viewportDriverSwitchoff = viewportDriver.variables.new()
+            viewportDriverSwitchoff.name = "LODRIVERSWITCHOFF"
+            viewportDriverSwitchoff.targets[0].id = lodObject
+            viewportDriverSwitchoff.targets[0].data_path = '["LODRIVER_SWITCHOFF"]'
             viewportDriverDistanceToRefVar = viewportDriver.variables.new()
             viewportDriverDistanceToRefVar.name = "LODRIVERDISTTOREF"
             viewportDriverDistanceToRefVar.type = 'LOC_DIFF'
             viewportDriverDistanceToRefVar.targets[0].id = distanceReferenceEmpty
             viewportDriverDistanceToRefVar.targets[1].id = lodObject
-            viewportDriver.expression = "LODRIVERDISTTOREF > LODRIVERMAX or LODRIVERDISTTOREF < LODRIVERMIN"
+            viewportDriver.expression = "(LODRIVERDISTTOREF > LODRIVERMAX or LODRIVERDISTTOREF < LODRIVERMIN) and LODRIVERSWITCHOFF == 0"
             renderDriver = lodObject.driver_add("hide_render").driver
             renderDriverCopyViewportVar = renderDriver.variables.new()
             renderDriverCopyViewportVar.name = "LODRIVERCOPYVIEWPORT"
             renderDriverCopyViewportVar.targets[0].id = lodObject
             renderDriverCopyViewportVar.targets[0].data_path = 'hide_viewport'
             renderDriver.expression = "LODRIVERCOPYVIEWPORT"
-
-            
-            print(lodObject.name)
+        return {'FINISHED'}
+    
+#function to add empties at the origins of selected objects, then parent the objects to the empties
+class LODRIVER_OT_ParentToEmpties(bpy.types.Operator):
+    bl_idname = "lodriver.parenttoempties"
+    bl_label = "Parent selected objects to empties"
+    bl_description = "Add an empty at the origin of each selected object, then parent the object to that empty"
+    
+    def execute(self, context):
+        #store selected objects
+        selectedObjects = bpy.context.selected_objects
+        #step through all selected objects to add empties and parent the objects to those empties
+        for objectToParent in selectedObjects:
+            bpy.ops.object.empty_add(type='PLAIN_AXES',location=objectToParent.location)
+            parentEmpty = bpy.context.selected_objects[0]
+            parentEmpty.name = "LODRIVER_TRANSFORM_" + objectToParent.name
+            objectToParent.parent = parentEmpty
+            objectToParent.matrix_parent_inverse
+            objectToParent.location = (0,0,0)
+        return {'FINISHED'}
+    
+#function to override drivers to make objects visible for easier duplication
+class LODRIVER_OT_VisibleOverrideOn(bpy.types.Operator):
+    bl_idname = "lodriver.visibleoverrideon"
+    bl_label = "Turn on visibile override"
+    bl_description = "Set a property so that the drivers for the active collection objects keep object visiblility on"
+    
+    def execute(self, context):
+        #store collection objects
+        collectionObjects = bpy.context.collection.objects
+        #step through all objects in active collection to add empties and parent the objects to those empties
+        for objectToMakeVisible in collectionObjects:
+            if("LODRIVER_SWITCHOFF" in objectToMakeVisible):
+                objectToMakeVisible["LODRIVER_SWITCHOFF"] = 1
+        return {'FINISHED'}
+    
+#function to clear override from drivers
+class LODRIVER_OT_VisibleOverrideOff(bpy.types.Operator):
+    bl_idname = "lodriver.visibleoverrideoff"
+    bl_label = "Turn off visibile override"
+    bl_description = "Clear the override property on the active collection objects so the drivers switch LODs as expected"
+    
+    def execute(self, context):
+        #store collection objects
+        collectionObjects = bpy.context.collection.objects
+        #step through all objects in active collection to add empties and parent the objects to those empties
+        for objectToRevertLOD in collectionObjects:
+            if("LODRIVER_SWITCHOFF" in objectToRevertLOD):
+                objectToRevertLOD["LODRIVER_SWITCHOFF"] = 0
         return {'FINISHED'}
              
 #register and unregister all LOD River classes
 lodriverClasses = (  LODRIVER_PT_LodPanel,
-                    LODRIVER_OT_CreateDrivers)
+                    LODRIVER_OT_CreateDrivers,
+                    LODRIVER_OT_ParentToEmpties,
+                    LODRIVER_OT_VisibleOverrideOn,
+                    LODRIVER_OT_VisibleOverrideOff)
 
 register, unregister = bpy.utils.register_classes_factory(lodriverClasses)
 
